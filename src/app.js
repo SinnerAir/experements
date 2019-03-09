4abc0792a97ad3cf775d9e4341711259529226a5
Airat <fam_89@mail.ru>
2019-03-09 11:42:08 +0400

    5

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ Vue.config.productionTip = false
 
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker
-    .register('/service-worker.js')
+    .register('service-worker.js')
     .then(() => {
       console.log('Service Worker Registered')
     })
